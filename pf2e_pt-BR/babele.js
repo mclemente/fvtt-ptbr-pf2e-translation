04106cf658565b57a7f5ac5ed48c18a5fa812265
Elvis Pereira <elvispereira@hotmail.com>
2021-04-10 17:00:36 -0300

    style: implement semistandard

--- pf2e_pt-BR/babele.js
+++ pf2e_pt-BR/babele.js
@@ -1,9 +1,11 @@
+/* global Hooks, Babele */
+
 Hooks.on('init', () => {
   if (typeof Babele !== 'undefined') {
     Babele.get().register({
       module: 'pf2e_pt-BR',
       lang: 'pt-BR',
       dir: 'compendium'
-    })
+    });
   }
-})
+});
