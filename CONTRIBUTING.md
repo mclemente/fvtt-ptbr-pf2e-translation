ad3344561fb391895f24d6da90e66b99e178002b
Matheus Clemente <matheus.xb@gmail.com>
2024-02-19 17:05:27 -0300

    Reorganizando arquivos

--- CONTRIBUTING.md
+++
@@ -1,5 +0,0 @@
-If you would like to contribute for the FoundryVTT Pathfinder 2e System localization into the Brazilian Portuguese language, you can do so in one of the following ways:
-
-1. Sending merge requests to this repository with the pt-BR.json file with its translated strings.
-
-2. Creating new issues in this repository containing any new translation or error that you found.
---
+++ gulpfile.mjs
@@ -0,0 +1,61 @@
+// SPDX-FileCopyrightText: 2022 Johannes Loher
+// SPDX-FileCopyrightText: 2022 David Archibald
+//
+// SPDX-License-Identifier: MIT
+
+import fs from "fs-extra";
+import gulp from "gulp";
+
+/********************/
+/*  CONFIGURATION   */
+/********************/
+
+const sourceDirectory = "./";
+const distDirectory = "./dist";
+const staticFiles = ["lang/pt-BR", "module.json"];
+
+/********************/
+/*      BUILD       */
+/********************/
+
+/**
+ * Copy static files
+ */
+async function copyFiles() {
+	for (const file of staticFiles) {
+		if (fs.existsSync(`${sourceDirectory}/${file}`)) {
+			await fs.copy(`${sourceDirectory}/${file}`, `${distDirectory}/${file}`);
+		}
+	}
+}
+
+/**
+ * Watch for changes for each build step
+ */
+export function watch() {
+	gulp.watch(
+		staticFiles.map((file) => `${sourceDirectory}/${file}`),
+		{ ignoreInitial: false },
+		copyFiles
+	);
+}
+
+export const build = gulp.series(clean, gulp.parallel(copyFiles));
+
+/********************/
+/*      CLEAN       */
+/********************/
+
+/**
+ * Remove built files from `dist` folder while ignoring source files
+ */
+export async function clean() {
+	const files = [...staticFiles, "module"];
+
+	console.log(" ", "Files to clean:");
+	console.log("   ", files.join("\n    "));
+
+	for (const filePath of files) {
+		await fs.remove(`${distDirectory}/${filePath}`);
+	}
+}
