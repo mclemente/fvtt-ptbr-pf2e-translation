a3aab13ff4d4a94622474e72573bbbaa309424c6
Matheus Clemente <matheus.xb@gmail.com>
2024-08-02 17:33:25 -0300

    Readicionar Babele

--- gulpfile.mjs
+++ gulpfile.mjs
@@ -5,6 +5,13 @@
 
 import fs from "fs-extra";
 import gulp from "gulp";
+import sourcemaps from "gulp-sourcemaps";
+import buffer from "vinyl-buffer";
+import source from "vinyl-source-stream";
+
+import rollupStream from "@rollup/stream";
+
+import rollupConfig from "./rollup.config.mjs";
 
 /********************/
 /*  CONFIGURATION   */
@@ -12,11 +19,29 @@ import gulp from "gulp";
 
 const sourceDirectory = ".";
 const distDirectory = "./dist";
+const sourceFileExtension = "js";
 const staticFiles = ["module.json"];
 
 /********************/
 /*      BUILD       */
 /********************/
+
+let cache;
+
+/**
+ * Build the distributable JavaScript code
+ */
+function buildCode() {
+	return rollupStream({ ...rollupConfig(), cache })
+		.on("bundle", (bundle) => {
+			cache = bundle;
+		})
+		.pipe(source(`module.js`))
+		.pipe(buffer())
+		.pipe(sourcemaps.init({ loadMaps: true }))
+		.pipe(sourcemaps.write("."))
+		.pipe(gulp.dest(`${distDirectory}/module`));
+}
 
 /**
  * Copy static files
@@ -36,6 +61,7 @@ async function copyFiles() {
  * Watch for changes for each build step
  */
 export function watch() {
+	gulp.watch(`${sourceDirectory}/src/**/*.${sourceFileExtension}`, { ignoreInitial: false }, buildCode);
 	gulp.watch(
 		staticFiles.map((file) => `${sourceDirectory}/${file}`),
 		{ ignoreInitial: false },
@@ -43,7 +69,7 @@ export function watch() {
 	);
 }
 
-export const build = gulp.series(clean, gulp.parallel(copyFiles));
+export const build = gulp.series(clean, gulp.parallel(buildCode, copyFiles));
 
 /********************/
 /*      CLEAN       */
---
+++ src/module/translator/translator.js
@@ -0,0 +1,429 @@
+import { TRANSLATOR_CONFIG } from "./translator-config.js";
+
+export class Translator {
+	static get() {
+		if (!Translator.instance) {
+			Translator.instance = new Translator();
+		}
+		return Translator.instance;
+	}
+
+	// Initialize translator
+	async initialize() {
+		// Read config file
+		const config = TRANSLATOR_CONFIG;
+
+		// Initialize artwork lists
+		this.artworkLists = {};
+
+		// Load translations from dictionary
+		const dictionaryPath = config?.paths?.dictionary ?? undefined;
+		if (dictionaryPath) {
+			const dict = await Promise.all([
+				fetch(dictionaryPath)
+					.then((result) => result.json())
+					.catch((error) => {
+						console.error(error);
+					}),
+			]);
+			this.dictionary = dict[0];
+		} else {
+			console.error("pathfinder-2e-pt-br: Dictionary not available");
+		}
+
+		// Create list of icons
+		this.icons = config?.iconList ?? {};
+
+		// Create item blacklist for items. Actor items with compendium sources on this list won't get synchronized with the compendium data
+		this.itemBlacklist = config?.itemBlacklist ?? [];
+
+		// Create list of mappings
+		this.mappings = config?.mappings ?? {};
+	}
+
+	constructor() {
+		this.initialize();
+	}
+
+	// Sluggify a string
+	sluggify(label) {
+		return label
+			.replace(/([a-z])([A-Z])\B/g, "$1-$2")
+			.toLowerCase()
+			.replace(/['’]/g, "")
+			.replace(/[^a-z0-9]+/gi, " ")
+			.trim()
+			.replace(/[-\s]+/g, "-");
+	}
+
+	// Get mapping
+	getMapping(mapping, compendium = false) {
+		const CompendiumMapping = game.babele.packs.entries().next().value[1].mapping.constructor;
+		if (compendium) {
+			return this.mappings[mapping]
+				? new CompendiumMapping(this.mappings[mapping].entryType, this.mappings[mapping].mappingEntries)
+				: {};
+		}
+		return this.mappings[mapping];
+	}
+
+	// Merge an object using a provided field mapping
+	dynamicMerge(sourceObject, translation, mapping) {
+		if (translation) {
+			foundry.utils.mergeObject(sourceObject, mapping.map(sourceObject, translation ?? {}), { overwrite: true });
+		}
+		return sourceObject;
+	}
+
+	// Merge an array of objects using a provided field mapping
+	dynamicArrayMerge(sourceArray, translations, mapping) {
+		// Loop through array, merge available objects
+		const mappedObjectArray = [];
+		for (let i = 0; i < sourceArray.length; i++) {
+			if (translations[i]) {
+				mappedObjectArray.push(this.dynamicMerge(sourceArray[i], translations[i], mapping));
+			} else {
+				mappedObjectArray.push(sourceArray[i]);
+			}
+		}
+		return mappedObjectArray;
+	}
+
+	// Merge an object list using a provided field mapping
+	dynamicObjectListMerge(sourceObjectList, translations, mapping) {
+		if (translations) {
+			const mergedObjectList = {};
+			Object.keys(sourceObjectList).forEach((entry) => {
+				Object.assign(mergedObjectList, {
+					[entry]: this.dynamicMerge(sourceObjectList[entry], translations[entry], mapping),
+				});
+			});
+		}
+	}
+
+	// Check if strike is ranged or melee and return the type
+	checkStrikeType(strike) {
+		let strikeType = "strike-melee";
+		strike.system.traits.value.forEach((trait) => {
+			if (trait.startsWith("range-") || trait.startsWith("thrown-")) {
+				strikeType = "strike-ranged";
+			}
+		});
+		return strikeType;
+	}
+
+	// Normalize name for correct display within Foundry
+	normalizeName(name) {
+		return name.replace("ß", "ss");
+	}
+
+	// Translate an array of similar objects, e.g. scenes or journal pages. This supports duplicate object names within the array using ids to identify the correct data
+	translateArrayOfObjects(data, translation, mappingType) {
+		data.forEach((entry, index, arr) => {
+			let objectTranslation = translation ? translation[entry.name] ?? undefined : undefined;
+
+			// Check if the object translation is an array (in case of duplicate names)
+			// Take the objectTranslation that matches the current objects' id
+			if (Array.isArray(objectTranslation)) {
+				objectTranslation = objectTranslation.find((obj) => obj.id === entry._id) ?? false;
+			}
+			if (objectTranslation) {
+				// For adventure journal pages, normalize the name
+				if (mappingType === "adventureJournalPage") {
+					objectTranslation.name = this.normalizeName(objectTranslation.name);
+				}
+				this.dynamicMerge(arr[index], objectTranslation, this.getMapping(mappingType, true));
+			}
+		});
+		return data;
+	}
+
+	// If an actor description format is provided create formatted html, otherwise use plain text
+	translateActorDescription(data, translation) {
+		if (translation) {
+			try {
+				JSON.parse(translation);
+			} catch (e) {
+				return translation;
+			}
+			const descriptionData = JSON.parse(translation);
+
+			let actorDescription = "";
+
+			// If actor description is available create actor name and actor description
+			if (descriptionData.ActorDescription) {
+				actorDescription = descriptionData.ActorName ? `<h2>${descriptionData.ActorName}</h2>` : "";
+				actorDescription = actorDescription.concat(descriptionData.ActorDescription);
+			}
+
+			// Create creature family name
+			if (descriptionData.FamilyName) {
+				actorDescription = actorDescription.concat(`<p>&nbsp;</p><h2>${descriptionData.FamilyName}</h2>`);
+
+				// If family name exists, create creature family description
+				if (descriptionData.FamilyDescription)
+					actorDescription = actorDescription.concat(descriptionData.FamilyDescription);
+			}
+
+			// Create additional infos
+			if (descriptionData.AdditionalInfo) {
+				actorDescription = actorDescription.concat(`<p>&nbsp;</p><table border="0"><tbody>`);
+
+				for (const [infoTypeNumbered, infos] of Object.entries(descriptionData.AdditionalInfo)) {
+					const infoType = infoTypeNumbered.slice(0, infoTypeNumbered.length - 1);
+					if (["item", "lore", "location", "monster", "rule", "treasure"].includes(infoType)) {
+						const img = this.icons[infoType]
+							? `<img src="${this.icons[infoType]}" alt="" width="40" height="40" />`
+							: " ";
+
+						for (const [infoName, infoText] of Object.entries(infos)) {
+							actorDescription = actorDescription
+								.concat(`<tr><td style="width: 45px" valign= "top">${img}</td>`)
+								.concat(`<td><h3>${infoName}</h3>${infoText}</td></tr>`);
+						}
+					}
+				}
+
+				actorDescription = actorDescription.concat(`</tbody></table>`);
+			}
+			return actorDescription;
+		}
+		return data;
+	}
+
+	// Return either localized or both localized and english text, based on module setting
+	translateDualLanguage(data, translation) {
+		if (!translation || data === translation) {
+			return data;
+		} else if (game.settings.get("pathfinder-2e-pt-br", "dual-language-names")) {
+			return this.normalizeName(translation) + "/" + data;
+		} else {
+			return this.normalizeName(translation);
+		}
+	}
+
+	// Translate heightened spells
+	translateHeightening(data, translation) {
+		if (data.levels) {
+			if (translation) {
+				foundry.utils.mergeObject(data.levels, translation, { overwrite: true });
+			}
+			Object.keys(data.levels).forEach((level) => {
+				["duration", "range", "time"].forEach((fieldName) => {
+					if (data.levels[level][fieldName]?.value) {
+						data.levels[level][fieldName].value = this.translateValue(
+							fieldName,
+							data.levels[level][fieldName].value
+						);
+					}
+				});
+			});
+		}
+		return data;
+	}
+
+	translateItems(data, translation, actorItem = false, mergeFromCompendium = true) {
+		data.forEach((entry, index, arr) => {
+			// Get the available translation for the item and the sluggified item name
+			let itemKey;
+
+			// Build itemKey depending on the type of item (world item or actor item)
+			if (actorItem) {
+				itemKey =
+					entry.type != "melee"
+						? `${entry.type}->${entry.name}`
+						: `${this.checkStrikeType(entry)}->${entry.name}`;
+			} else {
+				itemKey = entry.name;
+			}
+			let itemTranslation = translation ? translation[itemKey] ?? undefined : undefined;
+			let itemName = entry.name;
+
+			// For compendium items, get the data from the compendium
+			if (
+				entry.flags?.core?.sourceId &&
+				entry.flags.core.sourceId.startsWith("Compendium.pf2e.") &&
+				!entry.flags.core.sourceId.includes(".Actor.") &&
+				!this.itemBlacklist.includes(entry.flags.core.sourceId)
+			) {
+				// Get the actual compendium name
+				const itemCompendium = entry.flags.core.sourceId.split(".");
+
+				const originalName = fromUuidSync(entry.flags.core.sourceId)?.flags?.babele?.originalName;
+				if (originalName) {
+					entry.name = originalName;
+					itemName = originalName;
+
+					// Get the item from the compendium
+					const itemData = game.babele.packs
+						.get(`${itemCompendium[1]}.${itemCompendium[2]}`)
+						.translate(entry);
+
+					if (mergeFromCompendium) {
+						arr[index] = itemData;
+					} else {
+						arr[index].system.description.value = itemData.system.description.value;
+						arr[index].name = itemData.name;
+					}
+
+					// Remove dual language translations
+					if (arr[index].name.search("/") != -1) {
+						arr[index].name = arr[index].name.substring(0, arr[index].name.search("/"));
+					}
+				}
+			}
+
+			// Check if the item translation is an array (in case of duplicate items such as a magical and a regular shortsword)
+			// Take the itemTranslation that matches the current item's id
+			if (Array.isArray(itemTranslation)) {
+				itemTranslation = itemTranslation.find((itm) => itm.id === entry._id) ?? false;
+			}
+
+			// Merge the available translation
+			if (itemTranslation) {
+				// Normalize item name
+				if (itemTranslation.name) {
+					itemTranslation.name = this.normalizeName(itemTranslation.name);
+				}
+				// For name and description fields, replace "<Compendium>" tag with text from compendium if translation is provided
+				["description", "name"].forEach((dataElement) => {
+					if (itemTranslation[dataElement]) {
+						if (itemTranslation[dataElement].startsWith("<Compendium> tag will get replaced")) {
+							delete itemTranslation[dataElement];
+						} else {
+							itemTranslation[dataElement] =
+								dataElement === "description"
+									? itemTranslation[dataElement].replace(
+										  "<Compendium>",
+										  arr[index].system.description.value
+									  )
+									: itemTranslation[dataElement].replace("<Compendium>", arr[index].name);
+						}
+					}
+				});
+
+				this.dynamicMerge(arr[index], itemTranslation, this.getMapping("item", true));
+
+				// Add babele standard translated fields
+				foundry.utils.mergeObject(arr[index], {
+					translated: true,
+					hasTranslation: true,
+					originalName: itemName,
+					flags: {
+						babele: {
+							translated: true,
+							hasTranslation: true,
+							originalName: itemName,
+						},
+					},
+				});
+
+				// Translate available rules
+				if (itemTranslation.rules) {
+					arr[index].system.rules = this.translateRules(entry.system.rules, itemTranslation.rules);
+				}
+			}
+
+			// Add the item slug if not already included
+			if (!arr[index].system.slug || arr[index].system.slug === "") {
+				arr[index].system.slug = this.sluggify(itemName);
+			}
+		});
+
+		return data;
+	}
+
+	// Translates a specified value within an object using the dictionary
+	translateObject(type, fieldName, sourceObject) {
+		const translatedObject = {};
+		for (const [objectKey, objectData] of Object.entries(sourceObject)) {
+			if (objectKey === fieldName) {
+				Object.assign(translatedObject, { [objectKey]: this.translateValue(type, objectData) });
+			} else {
+				Object.assign(translatedObject, { [objectKey]: objectData });
+			}
+		}
+		return translatedObject;
+	}
+
+	// Translates a specified value within a normalized object list using the dictionary
+	// Sample call: translateObjectList("resistance", "exceptions", value);
+	translateObjectList(type, fieldName, sourceObjectList) {
+		if (Array.isArray(sourceObjectList)) {
+			const translatedArray = [];
+			sourceObjectList.forEach((sourceObject) => {
+				translatedArray.push(this.translateObject(type, fieldName, sourceObject));
+			});
+			return translatedArray;
+		} else if (typeof sourceObjectList === "object") {
+			const translatedObjectList = {};
+			for (const [objectKey, objectData] of Object.entries(sourceObjectList)) {
+				Object.assign(translatedObjectList, { [objectKey]: this.translateObject(type, fieldName, objectData) });
+			}
+			return translatedObjectList;
+		}
+	}
+
+	// Translate text labels provided in rule elements
+	translateRules(data, translation) {
+		if (translation) {
+			// Translation for regular strings like labels
+			this.dynamicArrayMerge(data, translation, this.getMapping("rule", true));
+
+			// Translation for array of choices within ChoiceSet rule element and array of values within ItemAlteration rule element
+			for (let i = 0; i < data.length; i++) {
+				if (data[i].choices && translation[i]?.choices) {
+					this.dynamicArrayMerge(data[i].choices, translation[i].choices, this.getMapping("choice", true));
+				}
+				if (data[i].value && translation[i]?.value) {
+					this.dynamicArrayMerge(data[i].value, translation[i].value, this.getMapping("value", true));
+				}
+			}
+		}
+		return data;
+	}
+
+	// Use a unique token name if provided, otherwise use the translated actor name
+	translateTokenName(data, translation, translationObject) {
+		return translation ?? translationObject.name ?? data;
+	}
+
+	// Translates a value from the dictionary
+	translateValue(type, value) {
+		const convertedValue = String(value).toLowerCase();
+		if (
+			Object.keys(this.dictionary).includes(type) &&
+			Object.keys(this.dictionary[type]).includes(convertedValue)
+		) {
+			return this.dictionary[type][convertedValue];
+		}
+		return value;
+	}
+
+	// For default icons, update the image if included in the media path
+	updateImage(type, value, dataObject, translatedCompendium) {
+		// Get image source based on type
+		let imageSource = "";
+		if (type === "portrait") {
+			imageSource = dataObject.img;
+		} else if (type === "token") {
+			imageSource = dataObject.prototypeToken.texture.src;
+		} else {
+			return value;
+		}
+		// Check, if image source uses default image
+		if (!(imageSource.includes("systems/pf2e/icons") || imageSource.includes("icons/svg/mystery-man"))) {
+			return value;
+		}
+		const artworkList = this.artworkLists[translatedCompendium.metadata.name];
+		if (
+			dataObject.type === "npc" &&
+			artworkList &&
+			artworkList[this.sluggify(dataObject.name)] &&
+			["portrait", "token"].includes(type)
+		) {
+			return artworkList[this.sluggify(dataObject.name)][type] ?? value;
+		}
+		return value;
+	}
+}
